65ddb2b4a9740dfb09e5e27f89f3ea0776d27076
shithinshetty <shithinshetty07@gmail.com>
2024-02-10 14:02:07 +0530

    added db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+const uri = process.env.MONGODB_URL;
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((err) => console.log(err));
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Heyy Gurl");
+});
+console.log(process.env.MONGODB_URL);
+console.log("Hey");
+
 app.listen(3000, () => {
   console.log("Server is sucessfully Running on port 3000");
 });
