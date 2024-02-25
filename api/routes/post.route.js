c50fe295c07c85c3023dd8a80a030ae934bf7527
shithinshetty <shithinshetty07@gmail.com>
2024-02-25 22:41:48 +0530

    get posts route added

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { verifytoken } from "../utils/verifyUser.js";
-import { create } from "../controllers/post.controller.js";
+import { create, getPosts } from "../controllers/post.controller.js";
 const router = express.Router();
 
 router.post("/create", verifytoken, create);
-
+router.get("/getposts", getPosts);
 export default router;
